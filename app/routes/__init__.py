d7735024612716b2e3c640be7c0f4376fa953df9
LeHibouMT <nickylarson6665@gmail.com>
2024-11-27 04:08:29 +0100

    Local database + basic HTTP methods API for item class

---
+++ app/routes/__init__.py
@@ -0,0 +1,4 @@
+from .items import router as items_router
+from .assets.stocks import router as assets_router
+
+__all__ = ['items_router', 'assets_router']
---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+
+ALPHA_VANTAGE_BASE_URL = "https://www.alphavantage.co/query"
+ALPHA_VANTAGE_API_KEY = os.getenv("ALPHA_VANTAGE_API_KEY")
